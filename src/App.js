b1b4df08134b3cfb25667243e2ecf3144bda8360
allison.ferreira <allison.ferreira@bandtec.com.br>
2020-07-28 00:08:06 -0300

    Primeiro setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './components/Menu'
+
+function App() {
+  return (
+    <div style={{background: "#141414"}}>
+      <Menu />
+      AAAAAAA
+    </div>
+  );
+}
+
+export default App;
